f7654f41e7b6b9d506e9e2480540de7875d760df
dimonier <dimonier@gmail.com>
2025-01-12 11:08:40 +0300

    Set fast `large-v3-turbo` as the default Whisper model

--- batch-speech-to-text.py
+++ batch-speech-to-text.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+# Credits:
+#  https://github.com/openai/whisper
+#  dimonier@gmail.com / https://t.me/dimonier / https://github.com/dimonier/batch-speech-to-text
 # To install whisper, execute:
 # pip install openai-whisper
 # More info: https://github.com/openai/whisper
@@ -11,15 +15,19 @@ audio_folder = r'C:\ASR'
 # whisper_model = 'tiny' # Very fast and very inaccurate speech recognition
 # whisper_model = 'base' # Fast and fourius
 # whisper_model = 'small' # Worse than medium but still OK
-whisper_model = 'medium' # Good recognition results but too slow on CPU
+# whisper_model = 'medium' # Good recognition results but too slow on CPU
+whisper_model = 'large-v3-turbo' # Almost the same size as medium, but should give better result
 # whisper_model = 'large' # Use on GPU only
 
+
 text_language = 'ru' # Force usage of specified language
+device = 'cuda'
+#device = 'cpu'
 
 def main():
 
 # Audio files with specified extensions will be processed
-    audio_exts = ['mp3', 'aac', 'ogg']
+    audio_exts = ['mp3', 'aac', 'wav', 'ogg', 'm4a', 'mp4', 'opus', 'mov']
 
     print(f'Looking into "{audio_folder}"')
     os.chdir(audio_folder)
@@ -44,6 +52,7 @@ def process_audiofile(fname):
     fname_noext = fname[:-(len(fext)+1)]
 
     model = whisper.load_model(whisper_model)
+    model = model.to(device)
 
     result = model.transcribe(fname, verbose = True, language = text_language)
 
